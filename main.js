dc2001c6e3692d9faee4e88133ddbb0775a6a5f9
Pushkar Singh Rawat <pushkarsrawat@gmail.com>
2018-04-29 11:41:05 +0200

    2 checkin

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
 var http = require("http");
-var pg = require('pg');
-
-
-pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-	if(err) {
-    return console.error('Client error.', err);
-  }
-   client.query('SELECT * FROM your_table', function(err, result) {
-      done();
-      if(err) return console.error(err);
-      console.log(result.rows);
-   });
-});
 
 http.createServer(function(request,response){
 	
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hello World\n');
+	response.end('Hello World123\n');
 }).listen(8081);
 
 console.log("Server running at http://127.0.0.1:8081/")
